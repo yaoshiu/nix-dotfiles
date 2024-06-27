3d02da5bc56bdb725b53dbae400465274489952f
yaoshiu <yaoshiu@qq.com>
2024-06-27 20:25:10 +0800

    security: maybe it's better to leave the power relevant commands administrator only

--- NixOS/security/sudo/default.nix
+++ NixOS/security/sudo/default.nix
@@ -1,26 +1,26 @@
 { pkgs, ... }:
 {
-  security.sudo.extraRules = [
-    {
-      commands = [
-        {
-          command = "${pkgs.systemd}/bin/systemctl suspend";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "${pkgs.systemd}/bin/reboot";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "${pkgs.systemd}/bin/poweroff";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "${pkgs.systemd}/bin/shutdown";
-          options = [ "NOPASSWD" ];
-        }
-      ];
-      groups = [ "wheel" ];
-    }
-  ];
+  # security.sudo.extraRules = [
+  #   {
+  #     commands = [
+  #       {
+  #         command = "${pkgs.systemd}/bin/systemctl suspend";
+  #         options = [ "NOPASSWD" ];
+  #       }
+  #       {
+  #         command = "${pkgs.systemd}/bin/reboot";
+  #         options = [ "NOPASSWD" ];
+  #       }
+  #       {
+  #         command = "${pkgs.systemd}/bin/poweroff";
+  #         options = [ "NOPASSWD" ];
+  #       }
+  #       {
+  #         command = "${pkgs.systemd}/bin/shutdown";
+  #         options = [ "NOPASSWD" ];
+  #       }
+  #     ];
+  #     groups = [ "wheel" ];
+  #   }
+  # ];
 }
